f60b255732efd859b6d80ede6c35952c9e941937
viunow <vini.bracht1@gmail.com>
2021-12-09 14:54:44 -0300

    clean app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
-//nao consigo usar o flex-layout, nao compila
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
